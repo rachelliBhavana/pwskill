7dbd0fdfb350113e1b9bf53f6b7d1a1ffb0542ad
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2023-03-30 00:13:50 +0530

    Add files via upload

---
+++ 21_feb.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 1> What is Web Scraping? Why is it Used? Give three areas where Web Scraping is used to get data.
+
+# Web Scrapper is process of extracting information or data from websites using automated software
+# tools or scripts 
+# It involves analyzing the HTML code of a web page and extracting the relevant information from it.
+# Web scraping is used to collect large amounts of data from websites quickly and efficiently.
+# Web scraping is used for a variety of reasons, including market research, lead generation, 
+# and competitor analysis. 
+
+# 2> What are the different methods used for Web Scraping?
+# There are several methods and tools used for web scraping, 
+# depending on the complexity of the project and the data required. 
+# Some of the most common methods used for web scraping include:
+# HTML parsing: 
+# This method involves parsing the HTML code of a web page using libraries like Beautiful Soup or lxml in Python. 
+# It allows developers to extract specific elements from a web page, such as headings, links, and images.
+
+# 3> What is Beautiful Soup? Why is it used?
+# Beautiful Soup is a Python library used for web scraping purposes.
+# It is a powerful library that allows developers to parse HTML and XML documents, 
+# and extract useful information from them. Beautiful Soup provides a set of functions and methods for navigating, 
+# searching, and modifying the parse tree.
+
+# 4> Why is flask used in this Web Scraping project?
+# Flask is a popular Python web framework that is often used for building web applications and APIs. 
+# Flask is lightweight, flexible, and easy to use, making it a good choice for many web scraping projects.
+
+# 5> Write the names of AWS services used in this project. Also, explain the use of each service.
+# AWS Elastic Beanstalk is a fully managed service that makes it easy to deploy, manage, 
+# and scale web applications and services. 
+# Elastic Beanstalk supports a variety of programming languages 
+# and frameworks including Python, Ruby, Java, .NET, Node.js, and more.
+
+# Pipeline is a tool for building and deploying serverless applications on AWS. It provides a simple, 
+# streamlined workflow for building and deploying Lambda functions, 
+# as well as other AWS resources such as API Gateway endpoints, DynamoDB tables, and S3 buckets.
+
